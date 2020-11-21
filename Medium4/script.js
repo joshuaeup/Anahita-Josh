35612dc176224d6c6251fd3a6c902caff9189fda
Joshua Proctor <joshuaproctor@macbook-pro.lan>
2020-11-20 19:01:27 -0500

    Updated Hard and Medium

--- Medium4/script.js
+++ Medium4/script.js
@@ -1,35 +1,38 @@
 "use strict";
 
 class Car {
-    constructor(make, model, year, milage, color) {
+    constructor(make, model, year, mileage, color) {
         this.make = make;
         this.model = model;
         this.year = year;
-        this.milage = milage;
+        this.mileage = mileage;
         this.color = color;
     }
 
     driveToWork() {
-        console.log(
-            `old mileage: ${this.milage} | new mileagea: ${this.milage + 33}`
-        );
+        console.log(`old mileage: ${this.mileage}`);
+        this.mileage += 33;
+        console.log(`new mileagea: ${this.mileage}`);
     }
 
     driveAroundTheWorld() {
-        console.log(
-            `old mileage: ${this.milage} | new mileagea: ${this.milage + 24000}`
-        );
+        console.log(`old mileage: ${this.mileage}`);
+        this.mileage += 24000;
+        console.log(`new mileagea: ${this.mileage}`);
     }
 
     runErrands() {
-        console.log(
-            `old mileage: ${this.milage} | new mileagea: ${this.milage + 30}`
-        );
+        console.log(`old mileage: ${this.mileage} |`);
+        this.mileage += 30;
+        console.log(`new mileagea: ${this.mileage}`);
     }
 }
 
 const myCar = new Car("Dodge", "Charger", 2006, 2000, "White");
 
 myCar.driveToWork();
+console.log(" "); // Space
 myCar.driveAroundTheWorld();
+console.log(" "); // Space
 myCar.runErrands();
+console.log(" "); // Space
